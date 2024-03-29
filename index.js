2a3f165a2f743da09b34e69b9987d31e7c55e8b1
Isaias Ferreira <mitogamer617@gmail.com>
2024-03-29 16:17:31 -0300

    testes v2

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-const { Client, GatewayIntentBits, ApplicationCommandOptionType } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { REST } = require('discord.js');
 const { Routes } = require('discord-api-types/v9');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 let allowedRoles = ['1102772131944747088', '1102772128098566174', '1170534085769302027', '1096423219793231995'];
 
-client.on('ready', async () => {
+const token = process.env.TOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
+const deliveryChannelId = process.env.COMANDO_ID;
+const responseChannelId = process.env.ENTREGA_ID;
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+client.once('ready', async () => {
     try {
         console.log('Bot está online!');
-
+        
         await rest.put(
-            Routes.applicationGuildCommands(client.user.id, GUILD_ID),
+            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
             { body: commands },
         );
-
     } catch (error) {
         console.error('Erro ao registrar os comandos:', error);
     }
-
 });
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
-    const { commandName, options, member, channel } = interaction;
-    const deliveryChannelId = process.env.COMANDO_ID;
-    const responseChannelId = process.env.ENTREGA_ID;
+    const { commandName, options, member, channel, guild } = interaction;
 
-    if (channel.id !== deliveryChannelId)  {
-            return interaction.reply({ content: 'Você não tem permissão para usar este comando.', ephemeral: true });
-        }
+    if (channel.id !== deliveryChannelId) {
+        return interaction.reply({ content: 'Você não tem permissão para usar este comando.', ephemeral: true });
+    }
 
     if (commandName === 'entrega') {
         const hasPermission = member.roles.cache.some(role => allowedRoles.includes(role.id));
@@ -40,55 +44,43 @@ client.on('interactionCreate', async interaction => {
             return interaction.reply({ content: 'Você não tem permissão para usar este comando.', ephemeral: true });
         }
         const user = options.getUser('usuario');
-        const responseChannel = interaction.guild.channels.cache.get(responseChannelId);
+        const responseChannel = guild.channels.cache.get(responseChannelId);
         if (responseChannel) {
+            await interaction.deferReply();
             await responseChannel.send(`${user}, sua skin foi enviada com sucesso! Verifique sua caixa de presente no jogo.`);
+            await interaction.followUp('Mensagem de entrega enviada com sucesso!');
         } else {
             console.error('Canal de resposta não encontrado.');
         }
-        await interaction.reply('Mensagem de entrega enviada com sucesso!');
+    } else if (commandName === 'add-permissao') {
+        const role = options.getRole('role');
+        if (!role) {
+            return interaction.reply('Por favor, mencione um cargo válido.');
+        }
+        allowedRoles.push(role.id);
+        await interaction.reply(`Cargo adicionado às permissões: ${role.name}`);
+    } else if (commandName === 'remove-permissao') {
+        const role = options.getRole('role');
+        if (!role) {
+            return interaction.reply('Por favor, mencione um cargo válido.');
+        }
+        allowedRoles = allowedRoles.filter(id => id !== role.id);
+        await interaction.reply(`Cargo removido das permissões: ${role.name}`);
+    } else if (commandName === 'ver-permissoes') {
+        const roleNames = allowedRoles.map(roleId => {
+            const role = guild.roles.cache.get(roleId);
+            return role ? role.name : 'Cargo desconhecido';
+        });
+        await interaction.reply(`Cargos permitidos: ${roleNames.join(', ')}`);
     }
 });
 
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName, options, member } = interaction;
-
-  if (commandName === 'add-permissao') {
-      const role = options.getRole('role');
-      if (!role) {
-          return interaction.reply('Por favor, mencione um cargo válido.');
-      }
-      allowedRoles.push(role.id);
-      await interaction.reply(`Cargo adicionado às permissões: ${role.name}`);
-  } else if (commandName === 'remove-permissao') {
-      const role = options.getRole('role');
-      if (!role) {
-          return interaction.reply('Por favor, mencione um cargo válido.');
-      }
-      allowedRoles = allowedRoles.filter(id => id !== role.id);
-      await interaction.reply(`Cargo removido das permissões: ${role.name}`);
-  } else if (commandName === 'ver-permissoes') {
-      const roleNames = allowedRoles.map(roleId => {
-          const role = interaction.guild.roles.cache.get(roleId);
-          return role ? role.name : 'Cargo desconhecido';
-      });
-      await interaction.reply(`Cargos permitidos: ${roleNames.join(', ')}`);
-  }
-});
-
-const token = process.env.TOKEN
-const CLIENT_ID = process.env.CLIENT_ID
-const GUILD_ID = process.env.GUILD_ID
-
 const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-   {
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+    {
         name: 'entrega',
         description: 'Envia uma skin para um usuário.',
         options: [
@@ -101,51 +93,33 @@ const commands = [
         ],
     },
     {
-      name: 'add-permissao',
-      description: 'Adiciona um cargo às permissões.',
-      options: [
-          {
-              type: 8,
-              name: 'role',
-              description: 'nome do cargo',
-              required: true
-          }
-      ]
-  },
-  {
-      name: 'remove-permissao',
-      description: 'Remove um cargo das permissões.',
-      options: [
-          {
-              type: 8,
-              name: 'role',
-              description: 'nome do cargo',
-              required: true
-          }
-      ]
-  },
-  {
-      name: 'ver-permissoes',
-      description: 'Mostra a lista de cargos permitidos.'
-  }
-];
-
-const rest = new REST({ version: '9' }).setToken(token);
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    if (typeof CLIENT_ID === 'string' && typeof GUILD_ID === 'string') {
-      await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
-
-      console.log('Successfully reloaded application (/) commands.');
-    } else {
-      console.error('CLIENT_ID and GUILD_ID must be strings representing snowflake IDs.');
+        name: 'add-permissao',
+        description: 'Adiciona um cargo às permissões.',
+        options: [
+            {
+                type: 8,
+                name: 'role',
+                description: 'nome do cargo',
+                required: true
+            }
+        ]
+    },
+    {
+        name: 'remove-permissao',
+        description: 'Remove um cargo das permissões.',
+        options: [
+            {
+                type: 8,
+                name: 'role',
+                description: 'nome do cargo',
+                required: true
+            }
+        ]
+    },
+    {
+        name: 'ver-permissoes',
+        description: 'Mostra a lista de cargos permitidos.'
     }
-  } catch (error) {
-    console.error(error);
-  }
-})();
+];
 
 client.login(token);
